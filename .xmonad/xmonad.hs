2262b785a765e25df9c72181e939628143b00940
Bill Casarin <bill@casarin.ca>
2012-11-16 11:48:56 -0500

    I'm happy with alt-ctrl for now, may need to change it later

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,6 +2,6 @@ import XMonad
 import OSXMonad.Core
 
 main = osxmonad defaultConfig {
-           modMask = mod1Mask
+           modMask = mod1Mask .|. controlMask
          , keys = osxKeys
        }
