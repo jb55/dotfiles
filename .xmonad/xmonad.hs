fe5d40a018f8abc1a74fa0f075701baa25832c30
Bill Casarin <bill@casarin.ca>
2012-11-15 20:58:20 -0500

    Update xmonad config, simple shell

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,17 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Layout.NoBorders
-main = xmonad
+import XMonad.Util.EZConfig
+import XMonad.Prompt.Shell
+import XMonad.Prompt
+
+main = xmonad $
     gnomeConfig {
             terminal    = "term"
           , layoutHook  = smartBorders (layoutHook gnomeConfig)
     }
+    `additionalKeysP` myKeys
+
+myKeys = [
+    ("M-x", shellPrompt defaultXPConfig)
+  ]
