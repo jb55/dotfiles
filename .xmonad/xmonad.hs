6ee97c7732626abf3d802a381aea71f23ade4abb
Bill Casarin <bill@casarin.ca>
2012-11-16 10:48:30 -0500

    Just use default modMask

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,6 +2,6 @@ import XMonad
 import OSXMonad.Core
 
 main = osxmonad defaultConfig {
-         modMask = mod1Mask .|. mod4Mask,
-         keys = osxKeys
+           modMask = mod1Mask
+         , keys = osxKeys
        }
