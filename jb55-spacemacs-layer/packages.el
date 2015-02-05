bba4ddae06be7d501c6546b64e46b33408b7ddc7
William Casarin <bill@casarin.me>
2015-02-04 18:26:33 -0800

    add jade-mode and column indicator

--- jb55-spacemacs-layer/packages.el
+++ jb55-spacemacs-layer/packages.el
@@ -13,6 +13,8 @@
 (defvar jb55-packages
   '(
     csv-mode
+    jade-mode
+    fill-column-indicator
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
