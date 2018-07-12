9dd05cc7306faba5899261c5ad8c1933fb4cf495
William Casarin <jb55@jb55.com>
2018-07-12 10:40:44 -0700

    xmonad: disable pointer update again
    many windows freak out with this on

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -66,7 +66,7 @@ main = xmonad
      $ defaultConfig {
              terminal    = "urxvtc"
            , modMask     = mod4Mask
-           , logHook     = updatePointer (1 / 2, 1 / 2) (0, 0)
+           --, logHook     = updatePointer (1 / 2, 1 / 2) (0, 0)
            , layoutHook  = layout
            , startupHook = setWMName "LG3D"
            , manageHook  = manageDocks
