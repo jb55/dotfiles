861c1274caa41a4af0aed402c04e6aa338d34b56
William Casarin <jb55@jb55.com>
2018-09-03 23:14:07 -0700

    xmonad: various tweaks

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,23 +4,26 @@ import Data.Ratio
 import XMonad
 import XMonad.Actions.CycleWS
 import XMonad.Actions.UpdatePointer
+import XMonad.Actions.SpawnOn (shellPromptHere, manageSpawn)
 import XMonad.Hooks.EwmhDesktops (ewmh)
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.UrgencyHook
+import XMonad.Layout.CenteredMaster
 import XMonad.Layout.Gaps
 import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.NoBorders
+import XMonad.Layout.ResizableTile
 import XMonad.Layout.Spacing
 import XMonad.Layout.Spiral
 import XMonad.Layout.ToggleLayouts (ToggleLayout(ToggleLayout))
 import XMonad.Prompt
 import XMonad.Prompt.Shell
 import XMonad.Util.EZConfig
-import XMonad.Util.Paste
-import XMonad.Hooks.UrgencyHook
 import XMonad.Util.NamedWindows
+import XMonad.Util.Paste
 import XMonad.Util.Run
 
 import qualified XMonad.StackSet as W
@@ -42,9 +45,13 @@ allGaps = (U, taffySize + if sideGaps then gapSize else 0) :
             if sideGaps then map (,gapSize) (enumFrom D)
                         else []
 
-baseLayout = Mirror (Tall 1 (3/100) (1/2))
-         ||| spiral (6/7)
-         ||| Full
+baseLayout =
+    let
+        tall = ResizableTall 1 (3/100) (1/2) []
+    in
+          tall
+      ||| Mirror tall
+-- ||| spiral (6/7)
 
 layout = -- gaps allGaps
        -- . noBorders
@@ -69,7 +76,7 @@ main = xmonad
            --, logHook     = updatePointer (1 / 2, 1 / 2) (0, 0)
            , layoutHook  = layout
            , startupHook = setWMName "LG3D"
-           , manageHook  = manageDocks
+           , manageHook  = manageSpawn <+> manageDocks
            , normalBorderColor = "#222"
            , focusedBorderColor = "#97C178"
      }
@@ -87,7 +94,7 @@ toggleFull = sendMessage (Toggle FULL)
 toggleMaximized = toggleGaps >> toggleFull
 
 myKeys = [
-    ("M-p", shellPrompt myXPConfig)
+    ("M-p", shellPromptHere myXPConfig)
   , ("M-a", focusUrgent)
   , ("M-d", toggleWS)
   , ("M-r", toggleFull)
