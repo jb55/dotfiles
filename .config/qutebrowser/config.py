393915214874c84565f1506662c64b52aa7c1178
William Casarin <jb55@jb55.com>
2018-11-24 13:01:50 -0800

    qutebrowser: a few updates

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -50,10 +50,12 @@ c.bindings.key_mappings = {'<Ctrl+6>': '<Ctrl+^>', '<Ctrl+Enter>': '<Ctrl+Return
 config.bind('d', 'scroll-page 0 0.5')
 config.bind('u', 'scroll-page 0 -0.5')
 config.bind('e', 'open-editor')
-config.bind('co', 'tab-only --keep-pinned')
+config.bind('co', 'tab-only')
 config.bind('gl', 'tab-move +')
 config.bind('gh', 'tab-move -')
+config.bind('gF', 'hint all tab-bg')
 
+config.set('downloads.position', 'bottom')
 config.set('tabs.select_on_remove', 'last-used')
 
 config.set('url.default_page', 'about:blank')
