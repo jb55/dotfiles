4d143e401aeaec012038efa3bea107efa7298bee
William Casarin <jb55@jb55.com>
2019-10-09 11:33:41 -0700

    emacs-diff: tweak background color
    Signed-off-by: William Casarin <jb55@jb55.com>

--- custom.el
+++ custom.el
@@ -299,4 +299,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(diff-refine-added ((t (:background "dark green"))))
  '(notmuch-search-unread-face ((t nil))))
