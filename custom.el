6774ec2696b5822b6206bd686069c6730cfb7abe
William Casarin <jb55@jb55.com>
2019-11-26 08:39:29 -0800

    custom.el: updates

--- custom.el
+++ custom.el
@@ -9,7 +9,7 @@
  '(ahs-default-range 'ahs-range-whole-buffer)
  '(ahs-idle-interval 0.25)
  '(ahs-idle-timer 0 t)
- '(ahs-inhibit-face-list nil t)
+ '(ahs-inhibit-face-list nil)
  '(ansi-color-faces-vector
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
@@ -35,6 +35,7 @@
  '(elm-indent-offset 4)
  '(epg-gpg-home-directory "~/.gnupg")
  '(eshell-prompt-function 'jb55/eshell-prompt)
+ '(evil-kill-on-visual-paste t)
  '(evil-shift-width 2)
  '(evil-want-Y-yank-to-eol t)
  '(expand-region-contract-fast-key "V")
@@ -44,11 +45,14 @@
  '(fill-column 70)
  '(flycheck-clang-include-path '("deps"))
  '(flycheck-clang-language-standard nil)
+ '(flycheck-display-errors-function 'flycheck-pos-tip-error-messages)
  '(flycheck-gcc-language-standard "c++11")
  '(flycheck-ghc-args '("-isrc"))
  '(flycheck-hlint-ignore-rules '("Eta reduce"))
+ '(flycheck-standard-error-navigation t)
  '(fringe-mode '(1 . 1) nil (fringe))
  '(global-hl-line-mode t)
+ '(gnus-select-method '(nntp "nntp.lore.kernel.org"))
  '(grep-find-ignored-directories
    '("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "node_modules"))
  '(haskell-font-lock-symbols nil)
@@ -69,7 +73,7 @@
  '(haskell-process-type 'ghci)
  '(haskell-stylish-on-save nil t)
  '(haskell-tags-on-save nil)
- '(helm-echo-input-in-header-line nil t)
+ '(helm-echo-input-in-header-line nil)
  '(helm-ff-skip-boring-files t)
  '(highlight-changes-colors '("#d33682" "#6c71c4"))
  '(highlight-symbol-colors
@@ -113,12 +117,11 @@
  '(js2-bounce-indent-p t)
  '(js2-strict-missing-semi-warning nil)
  '(ledger-reports
-   (quote
-    (("bal realized" "%(binary) -f %(ledger-file) bal -V ")
+   '(("bal realized" "%(binary) -f %(ledger-file) bal -V ")
      ("bal" "%(binary) -f %(ledger-file) bal")
      ("reg" "%(binary) -f %(ledger-file) reg")
      ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
-     ("account" "%(binary) -f %(ledger-file) reg %(account)"))))
+     ("account" "%(binary) -f %(ledger-file) reg %(account)")))
  '(link-hint-delete-trailing-paren t)
  '(linum-format " %7i ")
  '(magit-diff-use-overlays nil)
@@ -132,6 +135,7 @@
  '(main-line-color1 "#1E1E1E")
  '(main-line-color2 "#111111")
  '(main-line-separator-style 'chamfer)
+ '(markdown-hide-urls t)
  '(mastodon-instance-url "https://maly.io")
  '(message-auto-save-directory "~/mail/drafts")
  '(message-kill-buffer-on-exit t)
@@ -139,6 +143,7 @@
  '(message-send-mail-function 'message-send-mail-with-sendmail)
  '(message-sendmail-envelope-from 'header)
  '(message-sendmail-f-is-evil nil)
+ '(mm-html-inhibit-images t)
  '(mm-sign-option nil)
  '(mm-text-html-renderer 'w3m)
  '(mml-secure-key-preferences
@@ -169,68 +174,65 @@
      (:name "today" :query "date:today and tag:inbox" :key "1")
      (:name "rss" :query "tag:rss and tag:inbox and not tag:busy" :key "r" :sort-order from-ascending)
      (:name "rss-busy" :query "tag:rss and tag:inbox and tag:busy" :key "R" :sort-order from-ascending)
-     (:name "2-day" :query "date:yesterday.. and tag:inbox" :key "2")))
+     (:name "2-day" :query "date:yesterday.. and tag:inbox" :key "2")
+     (:name "work" :query "tag:inbox and tag:work" :key "w")))
  '(notmuch-search-line-faces
-   (quote
-    (("unread" . notmuch-search-unread-face)
-     ("flagged" . notmuch-search-flagged-face))))
+   '(("unread" . notmuch-search-unread-face)
+     ("flagged" . notmuch-search-flagged-face)))
  '(notmuch-search-oldest-first nil)
- '(notmuch-search-sort-order (quote newest-first))
+ '(notmuch-search-sort-order 'newest-first)
  '(notmuch-show-all-tags-list t)
  '(notmuch-show-insert-text/plain-hook
-   (quote
-    (notmuch-wash-convert-inline-patch-to-part notmuch-wash-wrap-long-lines notmuch-wash-tidy-citations notmuch-wash-elide-blank-lines notmuch-wash-excerpt-citations)))
+   '(notmuch-wash-convert-inline-patch-to-part notmuch-wash-wrap-long-lines notmuch-wash-tidy-citations notmuch-wash-elide-blank-lines notmuch-wash-excerpt-citations))
  '(notmuch-tag-formats
-   (quote
-    (("unread" "?"
-      (propertize tag
-                  (quote face)
-                  (quote notmuch-tag-unread)))
-     ("flagged" "*"
+   '(("unread" "?"
+      (propertize tag 'face 'notmuch-tag-unread))
+     ("flagged"
+      #("*" 0 1
+        (face ((:foreground "gold"))))
       (notmuch-apply-face tag
-                          (quote
-                           (:foreground "gold"))))
+                          '(:foreground "gold")))
      ("pr"
       (notmuch-apply-face tag
-                          (quote
-                           (:foreground "green"))))
+                          '(:foreground "green")))
      ("issue"
       (notmuch-apply-face tag
-                          (quote
-                           (:foreground "orange"))))
-     ("filed" "f"
+                          '(:foreground "orange")))
+     ("filed"
+      #("f" 0 1
+        (face ((:foreground "dim gray"))))
       (notmuch-apply-face tag
-                          (quote
-                           (:foreground "dim gray"))))
+                          '(:foreground "dim gray")))
      ("list")
-     ("github" "gh"
+     ("github"
+      #("gh" 0 2
+        (face ((:foreground "dim gray"))))
       (notmuch-apply-face tag
-                          (quote
-                           (:foreground "dim gray"))))
-     ("inbox" "i"
+                          '(:foreground "dim gray")))
+     ("inbox"
+      #("i" 0 1
+        (face ((:foreground "white"))))
       (notmuch-apply-face tag
-                          (quote
-                           (:foreground "white"))))
+                          '(:foreground "white")))
      ("closed"
       (notmuch-apply-face tag
-                          (quote
-                           (:foreground "wheat"))))
+                          '(:foreground "wheat")))
      ("merged"
       (notmuch-apply-face tag
-                          (quote
-                           (:foreground "yellow"))))
+                          '(:foreground "yellow")))
      ("bot"
       (notmuch-apply-face tag
-                          (quote
-                           (:foreground "dim gray"))))
-     ("busy" "b"
+                          '(:foreground "dim gray")))
+     ("busy"
+      #("b" 0 1
+        (face ((:foreground "brown"))))
       (notmuch-apply-face tag
-                          (quote
-                           (:foreground "brown"))))
-     ("bitcoin" "btc"
+                          '(:foreground "brown")))
+     ("bitcoin"
+      #("btc" 0 3
+        (face ((:foreground "gold"))))
       (notmuch-apply-face tag
-                          (quote
-                           (:foreground "gold")))))))
+                          '(:foreground "gold")))))
  '(notmuch-wash-wrap-lines-length 80)
  '(olivetti-body-width 100 t)
  '(org-adapt-indentation nil)
@@ -244,7 +246,7 @@
      ""
      (800 1000 1200 1400 1600 1800 2000)))
  '(org-archive-location "archive/%s_archive::")
- '(org-directory "~/docs/org" t)
+ '(org-directory "~/docs/org")
  '(org-duration-format '((special . h:mm)))
  '(org-export-headline-levels 2)
  '(org-export-with-section-numbers nil)
@@ -258,7 +260,7 @@
  '(org-refile-targets '((org-agenda-files :maxlevel . 1)))
  '(org-use-sub-superscripts '{})
  '(package-selected-packages
-   '(ini-mode vi-tilde-fringe org-bullets helm-xref helm-themes helm-swoop helm-purpose helm-projectile helm-mode-manager helm-flx helm-descbinds helm-ag ace-jump-helm-line yasnippet-snippets yapfify yaml-mode ws-butler winum which-key wgrep weechat web-mode web-beautify w3m volatile-highlights uuidgen use-package twittering-mode toml-mode toc-org tide tagedit symon string-inflection sql-indent spotify spaceline-all-the-icons smex smeargle slim-mode shen-mode shen-elisp scss-mode sass-mode restart-emacs request rainbow-delimiters racket-mode racer pyvenv pytest pyenv-mode py-isort pug-mode psci psc-ide powershell popwin pippel pipenv pip-requirements persp-mode pcre2el password-generator paradox overseer orgit org-projectile org-present org-pomodoro org-mime org-download org-clock-csv org-brain open-junk-file omnisharp ob-sml nix-mode neotree nameless mvn move-text mmm-mode meghanada maven-test-mode mastodon markdown-toc magit-svn magit-gitflow macrostep lorem-ipsum livid-mode live-py-mode link-hint ledger-mode json-navigator json-mode js2-refactor js-doc jade-mode ivy-yasnippet ivy-xref ivy-rtags ivy-purpose ivy-hydra intero insert-shebang indent-guide importmagic impatient-mode idris-mode hungry-delete hlint-refactor hl-todo hindent highlight-parentheses highlight-numbers highlight-indentation helm-pages helm-make haskell-snippets groovy-mode groovy-imports graphviz-dot-mode gradle-mode google-translate google-c-style golden-ratio godoctor go-tag go-rename go-impl go-guru go-gen-test go-fill-struct go-eldoc gnuplot gnu-apl-mode glsl-mode gitignore-templates gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md ggtags fuzzy fsharp-mode font-lock+ flycheck-rust flycheck-rtags flycheck-pos-tip flycheck-ledger flycheck-haskell flycheck-elm flycheck-bashate flx-ido fish-mode fill-column-indicator fancy-battery eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-escape evil-cleverparens evil-args evil-anzu eval-sexp-fu ereader ensime emojify emoji-cheat-sheet-plus emmet-mode elm-mode elisp-slime-nav editorconfig dumb-jump dotenv-mode disaster diminish define-word dante cython-mode csv-mode counsel-projectile counsel-notmuch counsel-gtags counsel-css company-web company-tern company-statistics company-shell company-rtags company-nixos-options company-lua company-irony company-go company-ghci company-ghc company-emoji company-emacs-eclim company-cabal company-c-headers company-auctex company-anaconda column-enforce-mode cmm-mode clean-aindent-mode clang-format centered-cursor-mode cargo bison-mode base16-theme auto-yasnippet auto-highlight-symbol auto-compile aggressive-indent ace-window ace-link ac-ispell))
+   '(flycheck-ycmd company-ycmd ycmd request-deferred auctex-latexmk treemacs-magit attrap hybrid-mode lcr web-completion-data company-reftex anaconda-mode protobuf-mode ini-mode vi-tilde-fringe org-bullets helm-xref helm-themes helm-swoop helm-purpose helm-projectile helm-mode-manager helm-flx helm-descbinds helm-ag ace-jump-helm-line yasnippet-snippets yapfify yaml-mode ws-butler winum which-key wgrep weechat web-mode web-beautify w3m volatile-highlights uuidgen use-package twittering-mode toml-mode toc-org tide tagedit symon string-inflection sql-indent spotify spaceline-all-the-icons smex smeargle slim-mode shen-mode shen-elisp scss-mode sass-mode restart-emacs request rainbow-delimiters racket-mode racer pyvenv pytest pyenv-mode py-isort pug-mode psci psc-ide powershell popwin pippel pipenv pip-requirements persp-mode pcre2el password-generator paradox overseer orgit org-projectile org-present org-pomodoro org-mime org-download org-clock-csv org-brain open-junk-file omnisharp ob-sml nix-mode neotree nameless mvn move-text mmm-mode meghanada maven-test-mode mastodon markdown-toc magit-svn magit-gitflow macrostep lorem-ipsum livid-mode live-py-mode link-hint ledger-mode json-navigator json-mode js2-refactor js-doc jade-mode ivy-yasnippet ivy-xref ivy-rtags ivy-purpose ivy-hydra intero insert-shebang indent-guide importmagic impatient-mode idris-mode hungry-delete hlint-refactor hl-todo hindent highlight-parentheses highlight-numbers highlight-indentation helm-pages helm-make haskell-snippets groovy-mode groovy-imports graphviz-dot-mode gradle-mode google-translate google-c-style golden-ratio godoctor go-tag go-rename go-impl go-guru go-gen-test go-fill-struct go-eldoc gnuplot gnu-apl-mode glsl-mode gitignore-templates gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md ggtags fuzzy fsharp-mode font-lock+ flycheck-rust flycheck-rtags flycheck-pos-tip flycheck-ledger flycheck-haskell flycheck-elm flycheck-bashate flx-ido fish-mode fill-column-indicator fancy-battery eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-escape evil-cleverparens evil-args evil-anzu eval-sexp-fu ereader ensime emojify emoji-cheat-sheet-plus emmet-mode elm-mode elisp-slime-nav editorconfig dumb-jump dotenv-mode disaster diminish define-word dante cython-mode csv-mode counsel-projectile counsel-notmuch counsel-gtags counsel-css company-web company-tern company-statistics company-shell company-rtags company-nixos-options company-lua company-irony company-go company-ghci company-ghc company-emoji company-emacs-eclim company-cabal company-c-headers company-auctex company-anaconda column-enforce-mode cmm-mode clean-aindent-mode clang-format centered-cursor-mode cargo bison-mode base16-theme auto-yasnippet auto-highlight-symbol auto-compile aggressive-indent ace-window ace-link ac-ispell))
  '(pdf-view-midnight-colors '("#655370" . "#fbf8ef"))
  '(powerline-color1 "#1E1E1E")
  '(powerline-color2 "#111111")
@@ -268,6 +270,8 @@
  '(psc-ide-add-import-on-completion t t)
  '(psc-ide-rebuild-on-save nil t)
  '(python-indent-offset 4)
+ '(racer-command-timeout 0.2)
+ '(racer-eldoc-timeout 0.2)
  '(rainbow-identifiers-cie-l*a*b*-lightness 80)
  '(rainbow-identifiers-cie-l*a*b*-saturation 18)
  '(rcirc-buffer-maximum-lines 10000)
@@ -286,18 +290,22 @@
  '(smtpmail-smtp-service 587)
  '(standard-indent 4)
  '(tab-width 4)
- '(url-handler-mode t)
+ '(url-handler-mode nil)
  '(user-full-name "William Casarin")
  '(user-mail-address "jb55@jb55.com")
  '(vc-follow-symlinks t)
+ '(w3m-default-display-inline-images nil)
  '(weechat-auto-monitor-buffers
    '("monstercat.#general" "monstercat.#payments" "monstercat.#code"))
  '(weechat-auto-monitor-new-buffers t)
- '(weechat-modules '(weechat-button weechat-image weechat-complete)))
+ '(weechat-modules '(weechat-button weechat-image weechat-complete))
+ '(yas-snippet-dirs '("/home/jb55/.emacs.d/private/snippets/")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(diff-refine-added ((t (:background "dark green"))))
+ '(notmuch-message-summary-face ((t (:background "#353B45"))))
+ '(notmuch-search-non-matching-authors ((t (:foreground "#353B45"))))
  '(notmuch-search-unread-face ((t nil))))
